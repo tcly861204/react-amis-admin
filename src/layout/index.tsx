e1064b5a0a789dee724dc5d8a7e8df38d32f2734
tcly861204 <tcly861204@hotmail.com>
2024-03-08 13:18:57 +0800

    fix: 🐛 完善前端

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -4,6 +4,7 @@ import {
   IconMenuUnfold,
   IconMenuFold,
 } from '@arco-design/web-react/icon'
+import { useNavigate } from 'react-router-dom'
 import Sidebar from '@/components/Sidebar'
 import Toolbar from '@/components/ToolBar'
 import logoImg from '@/assets/logo.png'
@@ -13,14 +14,18 @@ import menuConfig from './menu'
 import styles from './index.module.scss'
 
 const App = () => {
+  const navigate = useNavigate()
   const { collapse, updateField } = useStore(state => state)
   const onCollapse = useCallback((collapsed: boolean) => {
     updateField('collapse', collapsed)
   }, [updateField])
+  const goHome = useCallback(() => {
+    navigate('/')
+  }, [navigate])
   return <Layout className={styles.container}>
     <Layout.Header>
       <div className={styles.navbar}>
-        <div className={styles.logo}>
+        <div className={styles.logo} onClick={goHome}>
           <h1><img src={logoImg} /><span>amis</span></h1>
         </div>
         <Toolbar />
